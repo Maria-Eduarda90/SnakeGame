7a5ae53fe1bc02b847da4bb2f4f327018411cd5e
maria <snowdennaruto90@gmail.com>
2023-08-12 18:42:43 -0400

    movendo objetos

--- index.py
+++ index.py
@@ -4,20 +4,29 @@ from sys import exit
 
 pygame.init()
 
+clock = pygame.time.Clock()
+
 largura = 640
 altura = 480
+
+x = largura / 2
+y = 0
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo")
 
 while True:
+    clock.tick(300)
+    tela.fill((0, 0, 0))
     for event in pygame.event.get():
         
         if event.type == QUIT:
             pygame.quit()
             exit()
-    
-    pygame.draw.rect(tela, (225, 0, 0), (200, 300, 40, 50))
-    pygame.draw.circle(tela, (134, 233, 45), (300, 260), 40)
-    pygame.draw.line(tela, (244, 120, 140), (390, 0), (390, 600), 5)
+            
+    if y >= altura:
+        y = 0
+            
+    y += 1
+    pygame.draw.rect(tela, (225, 0, 0), (x, y, 40, 50))
     pygame.display.update()
