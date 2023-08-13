f66337631646c8d9a8e4cec48b34f45ab838d971
maria <snowdennaruto90@gmail.com>
2023-08-13 17:30:50 -0400

    starting the snake game

--- test.py
+++ test.py
@@ -11,34 +11,29 @@ pygame.mixer.music.play(-1)
 
 barulho_colisao = pygame.mixer.Sound('smb_coin.wav')
 
+clock = pygame.time.Clock()
+
 largura = 640
 altura = 480
 
-x_snake = int(largura / 2)
-y_snake = int(altura / 2)
+x = int(largura / 2)
+y = int(altura / 2)
 
-x_maca = randint(40, 600)
-y_maca = randint(50, 430)
+x_azul = randint(40, 600)
+y_azul = randint(50, 430)
 
 pontos = 0
 fonte = pygame.font.SysFont("Arial", 40, True, True)
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo")
-clock = pygame.time.Clock()
-
-lista_snake = []
-
-def increase_snake(lista_snake):
-    for XeY in lista_snake:
-        pygame.draw.rect(tela, (0, 255, 0), (XeY[0], XeY[1], 20, 20))
 
 while True:
     clock.tick(300)
-    tela.fill((255, 255, 255))
+    tela.fill((0, 0, 0))
     
     mensagem = f'Pontos: {pontos}'
-    texto_formatado = fonte.render(mensagem, True, (0, 0, 0))
+    texto_formatado = fonte.render(mensagem, True, (255, 255, 255))
     
     for event in pygame.event.get():
         
@@ -60,29 +55,22 @@ while True:
         #         y += 20
         
         if pygame.key.get_pressed()[K_a]:
-            x_snake -= 20
+            x -= 20
         if pygame.key.get_pressed()[K_d]:
-            x_snake += 20
+            x += 20
         if pygame.key.get_pressed()[K_w]:
-            y_snake -= 20
+            y -= 20
         if pygame.key.get_pressed()[K_s]:
-            y_snake += 20
+            y += 20
               
-    snake = pygame.draw.rect(tela, (0, 255, 0), (x_snake, y_snake, 20, 20))
-    maca = pygame.draw.rect(tela, (255, 0, 0), (x_maca, y_maca, 20, 20))
+    ret_vermelho = pygame.draw.rect(tela, (225, 0, 0), (x, y, 40, 50))
+    ret_azul = pygame.draw.rect(tela, (0, 0, 255), (x_azul, y_azul, 40, 50))
     
-    if snake.colliderect(maca):
-        x_maca = randint(40, 600)
-        y_maca = randint(50, 430)
+    if ret_vermelho.colliderect(ret_azul):
+        x_azul = randint(40, 600)
+        y_azul = randint(50, 430)
         pontos += 1
         barulho_colisao.play()
-        
-    lista_cabeca = []
-    lista_cabeca.append(x_snake)
-    lista_cabeca.append(y_snake)
-    lista_snake.append(lista_cabeca)
-    
-    increase_snake(lista_snake)
         
     tela.blit(texto_formatado, (450, 40))
     
